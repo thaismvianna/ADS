c5f2c89063b4dd96cb5309ce379e8459a3d151d7
thaís <thaismvianna@outlook.com>
2026-02-19 17:11:44 -0300

    conhecendo biblioteca e entendendo a necessidade de recompilar

--- logica-em-C/estudo-extra/aula1.1.c
+++ logica-em-C/estudo-extra/aula1.1.c
@@ -1,5 +1,8 @@
 #include <stdio.h> //studio.h é a biblioteca
+#include <stdlib.h> //biblioteca para usar no windows
 
 int main(){
 	printf("Hello, world!\n");	//mostra a saída
+	
+	system("pause"); //quando imprimir na tela, o usuario tem que pressionar uma tecla para que o programa continue
 }
