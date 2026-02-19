211bc94e57b2922268d3af0fac84698815abfe98
thaís <thaismvianna@outlook.com>
2026-02-19 18:06:19 -0300

    debugando

---
+++ logica-em-C/estudo-extra/aula1.2/aula1.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//praticando a debugação
+
+int main(int argc, char *argv[]){
+	
+	printf("Mensagem 1.\n");
+	printf("Mensagem 2.\n");
+	printf("Mensagem 3.\n");
+	printf("Mensagem 4.\n");
+	printf("Mensagem 5.\n");
+	return 0;
+}
