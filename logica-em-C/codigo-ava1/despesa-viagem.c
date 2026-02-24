7655f4d125c47ae3b86faa80fb270dc951134fa5
thaís <thaismvianna@outlook.com>
2026-02-24 00:25:17 -0300

    pratica

---
+++ logica-em-C/codigo-ava1/despesa-viagem.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() { //variáveis 
+    float distancia;
+    float preco;
+    float gasto_total;
+    
+    printf("Digite a distancia da viagem em quilometros: ");
+    scanf("%f", &distancia);
+    
+    printf("Digite o preco do litro do combustivel: R$ ");
+    scanf("%f", &preco);
+    
+    //10 km por litro
+    // litro 6,33
+    gasto_total = (distancia / 10) * preco;
+
+    //obs: sempre para executar o codigo tem que compilar
+    
+    printf("\nRESULTADO DA VIAGEM\n");
+    printf("Distancia: %.2f km\n", distancia);
+    printf("Preco do litro: R$ %.2f\n", preco);
+    printf("Total a gastar: R$ %.2f\n", gasto_total);
+    
+    return 0;
+}
+
