3305a9341834f94d93382c11d3e6e52c68066ebd
thaís <thaismvianna@outlook.com>
2026-03-11 11:36:36 -0300

    correção

--- logica-em-C/codigo-ava2/thaisVianna.c
+++ logica-em-C/codigo-ava2/thaisVianna.c
@@ -3,7 +3,7 @@
 int main() {
     float peso;
     float altura;
-    float resultado_do_imc;
+    float resultado_imc;
 
     printf("Calculo de IMC\n");
     printf("Digite o seu peso em quilos: ");
@@ -12,9 +12,9 @@ int main() {
     printf("Digite a sua altura em metros e use ponto ao inves de virgula, por exemplo: 1.55: ");
     scanf("%f", &altura);
     
-    resultado_do_imc = peso / (altura * altura);
+    resultado_imc = peso / (altura * altura);
     
-    printf("\nSeu IMC: %.2f\n", resultado_do_imc);
+    printf("\nSeu IMC: %.2f\n", resultado_imc);
     
     return 0;
 }
